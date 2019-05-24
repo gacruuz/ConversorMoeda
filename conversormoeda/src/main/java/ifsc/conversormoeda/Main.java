7191a444181fd70696a9855f7c77f11c47653632
gacruuz <49540420+gacruuz@users.noreply.github.com>
2019-05-24 15:30:00 -0300

    Alteracao ações dos botoes.

--- conversormoeda/src/main/java/ifsc/conversormoeda/Main.java
+++ conversormoeda/src/main/java/ifsc/conversormoeda/Main.java
@@ -3,8 +3,6 @@ package ifsc.conversormoeda;
 import Auxiliar.AlertaErro;
 import Janelas.Menu;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -57,17 +55,7 @@ public class Main extends Application {
 		//then you set to your node
 		pane.setBackground(new Background(myBI));
 
-		Button button1 = new Button("LOGAR");
-		button1.setFont(Font.font("Bebas", FontWeight.BOLD, 12));
-		button1.setLayoutX(175);
-		button1.setLayoutY(250);
-		button1.setPrefWidth(60);
 
-		Button button2 = new Button("SAIR");
-		button2.setFont(Font.font("Bebas", FontWeight.BOLD, 12));
-		button2.setLayoutX(255);
-		button2.setLayoutY(250);
-		button2.setPrefWidth(60);
 
 		TextField usuario = new TextField();
 		usuario.setLayoutX(185);
@@ -79,47 +67,54 @@ public class Main extends Application {
 		senha.setLayoutY(200);
 		senha.setPrefWidth(120);
 
-		// -------- ACOES -------//
-		button1.setOnAction(new EventHandler<ActionEvent>() {
+		Button button1 = new Button("LOGAR");
+		button1.setFont(Font.font("Bebas", FontWeight.BOLD, 12));
+		button1.setLayoutX(175);
+		button1.setLayoutY(250);
+		button1.setPrefWidth(60);
+		button1.setOnAction(e -> botaologinacao(window, usuario, senha));
 		
-			@Override
-
-			public void handle(ActionEvent event) {
-				
-				if (usuario.getText().equals("admin") && senha.getText().equals("admin")) {
-					try {
-						new Menu().start(window);
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				
-				}else {
-					
-						try {
-							new AlertaErro().start(new Stage());
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					
-				}
-
-			}
-
-		});
-
-		button2.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				System.exit(0);
-			}
-		});
-
-		// -------- ACOES -------//
+		Button button2 = new Button("SAIR");
+		button2.setFont(Font.font("Bebas", FontWeight.BOLD, 12));
+		button2.setLayoutX(255);
+		button2.setLayoutY(250);
+		button2.setPrefWidth(60);
+		button2.setOnAction(e -> botaosairacao(window));
+		
+		
 
 		pane.getChildren().addAll(button1, button2, senha, usuario);
 
+	}
+	
+	private void botaologinacao(Stage window, TextField usuario, TextField senha) {
+		
+		if (usuario.getText().equals("admin") && senha.getText().equals("admin")) {
+			try {
+				new Menu().start(window);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}else {
+			
+				try {
+					new AlertaErro().start(new Stage());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+		}
+		
+	}
+	
+	private void botaosairacao(Stage window) {
+		
+			System.exit(0);
+		
+		
 	}
 
 }
